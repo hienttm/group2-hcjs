7fdee9f240592486ef06342cd018c901d15d67ae
huevu102 <huevt.jp@gmail.com>
2022-09-26 14:27:29 +0700

    commit code

--- js/jsstyle.js
+++ js/jsstyle.js
@@ -1,3 +1,4 @@
+// CHECK EMAIL INPUT
 function validateForm() {
     let x = document.forms["myForm"]["email"].value;
     if (x == "") {
@@ -7,7 +8,6 @@ function validateForm() {
         alert('Thanks for signing up!!\nWe will send you email about our updates.');
     }
 }
-
 function validateFormBottom() {
     let y = document.forms["myFormBottom"]["emailBottom"].value;
     if (y == "") {
@@ -16,4 +16,20 @@ function validateFormBottom() {
     }else{
         alert('Thanks for signing up!!\nWe will send you email about our updates.');
     }
+}
+
+// COLLAPSE-EXPAND CONTENT
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight){
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
 }
